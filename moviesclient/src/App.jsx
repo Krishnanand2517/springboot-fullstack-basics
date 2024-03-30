3a5c639132f1b41a2713526794f4172abc08ad0d
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-03-30 12:59:48 +0530

    Set up initial routes

--- moviesclient/src/App.jsx
+++ moviesclient/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import api from "./api/axiosConfig";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/home/Home";
 import "./App.css";
-import axios from "axios";
 
 function App() {
   const [movies, setMovies] = useState();
@@ -20,7 +22,15 @@ function App() {
     getMovies();
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
